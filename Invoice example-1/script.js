2164c3e127ef80caaf69842d5b3f41e7574d5eb0
GRISHMA\grish <grishma.dholakiya@gmail.com>
2023-02-25 09:07:04 -0500

    Invoice I made Inbox

--- Invoice example-1/script.js
+++ Invoice example-1/script.js
@@ -7,3 +7,44 @@ $(document).ready(function () {
     $(".tableTr").parent().append(html);
     $(".tableTr").remove();
 });
+
+function onClick() {
+    var name = $("#name").val();
+    $(".name").html(name);
+    $("#name").remove();
+    window.print();
+
+    var invoice = $("#invoice").val();
+    $(".invoice").html(invoice);
+    $("#invoice").remove();
+    window.print();
+
+    var Phone = $("#Phone").val();
+    $(".Phone").html(Phone);
+    $("#Phone").remove();
+    window.print();
+
+    var Email = $("#Email").val();
+    $(".Email").html(Email);
+    $(".Email").remove();
+    window.print();
+
+    var Date = $("#Date").val();
+    $("Date").html(Date);
+    $(".Date").remove();
+    window.print();
+
+    var Soldby = $("#Sold by").val();
+    $(".Sold by").html(Soldby);
+    $("#Sold by").remove();
+    window.print();
+
+    var DueDate = $("#Due Date").val();
+    $(".Due Date").html(DueDate);
+    $("#Due Date").remove();
+    window.print();
+
+
+
+
+}
